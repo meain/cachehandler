82c49d3555bf9517d2ef9832e054e8989e4daf75
Abin Simon <abinsimon10@gmail.com>
2016-07-05 21:13:33 +0530

    Updated cachehandler.py,wolftest.py

---
+++ wolftest.py
@@ -0,0 +1,16 @@
+from cachehandler import apihandler
+import wolframalpha
+from apikeys import *
+
+def main():
+    client = wolframalpha.Client(key_wolfram.key)
+    handle = apihandler()
+    '''
+    Making calls:
+        Original : venue.search(locality='New York')
+    '''
+    data = handle.api_call(client.query, 'New York')
+    print data
+
+if __name__ == '__main__':
+    main()
